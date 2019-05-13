0a07108f029b4531614a3936dd241dfce2d899d5
Krzysztof Królikowski <kkrolikowski@gmail.com>
2019-05-13 23:30:05 +0200

    set proper returning value

--- Updater.py
+++ Updater.py
@@ -2,15 +2,15 @@ import requests
 import json
 
 class ETCDClass:
-    nodes = []
-
     def __init__(self, etcdAddr, clName):
         self.etcdAddr = etcdAddr
         self.clName = clName
+        self.nodes = []
     def read(self):
         etcd = requests.get("http://" + self.etcdAddr + "/v2/keys/pxc-cluster/" + self.clName)
         if etcd.status_code == 200:
             etcd_json = json.loads(etcd.text)
             for sqlnodes in etcd_json['node']['nodes']:
                 hosts = sqlnodes['key'].split('/')
-                nodes.append(hosts[3])
+                self.nodes.append(hosts[3])
+        return self.nodes
